0ce41fa403a034730e9d53c7f0b884691db5cc15
Kumar Basnet <kumarkingxeetri100@gmail.com>
2025-04-11 13:47:19 +0545

    fixes bugs

--- script.js
+++ script.js
@@ -52,3 +52,30 @@ circles.forEach(circle => {
     let percent = circle.getAttribute('data-percent');
     circle.style.setProperty('--percent', percent);
 });
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_hfq28zn','template_5ykpgre','#contact-form','Dddfv9XJWK6flJNCW')
+    .then(()=>{
+        // Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        // Remove message after five seconds
+        setTimeout(()=>{
+           contactMessage.textContent = ''
+        }, 5000)
+
+        // Clear input fields
+        contactForm.reset()
+    }, ()=>{
+       // Show error message
+       contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail);
